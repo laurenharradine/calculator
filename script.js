d89447ce2a549378946ae1755379d8dd34b55f69
Lauren Harradine <lhharradine@gmail.com>
2023-01-29 15:39:05 +1100

    Uploading basic calc code.

---
+++ script.js
@@ -0,0 +1,68 @@
+let input = ""
+
+let btn = document.getElementsByClassName("button");
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', getButton, false);
+}
+
+function getButton (){
+    if (this.classList.contains("clear")){
+        console.log("Clearing...")
+        input = "0"
+    }
+    else if (this.classList.contains("equals")){
+        console.log("Time to evaluate...")
+        input = parseStr(input) // add function here to parse string
+    }
+    else if (!this.classList.contains("number")){
+        input += checkOperand(this)
+    }
+    else {
+        input += this.innerHTML
+    }
+    console.log (`Input: ${input}`)
+    let screen = document.querySelector(".calc-screen")
+    screen.innerHTML = input
+
+    // Start from zero if we just evaluated a string
+    if (this.classList.contains("equals")) input = ""
+}
+
+function checkOperand(op) {
+    let operand = ""
+    if (op.classList.contains("plus")){
+        console.log("Plus")
+        operand = "+"
+    }
+    else if (op.classList.contains("minus")){
+        console.log("Minus")
+        operand = "-"
+    }
+    else if (op.classList.contains("multiply")){
+        console.log("Multiply")
+        operand = "*"
+    }
+    else { //it's a divide
+        console.log("Divide")
+        operand = "/"
+    }
+    return operand
+}
+
+function parseStr (str){
+    // check if string begins wiht an operand; if so, unshift a zero onto front of string.
+    let strArr = str.split("")
+
+    if (!isNumber(Number(strArr[0]))){
+        strArr.unshift('0')
+        str=strArr.join("")
+    }
+
+    console.log(str)
+
+    return Function(`'use strict'; return (${str})`)()
+}
+
+function isNumber(char) {
+    return /^[0-9]+$/.test(char);
+}
